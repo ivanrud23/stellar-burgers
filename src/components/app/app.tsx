1eaa8a52a4c1d0c3d4317beef8acc3385ec5f46a
Ivan Rud <ivanrud23@yandex.ru>
2025-10-06 13:12:03 +0300

    Iter_3

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -113,7 +113,7 @@ const App = () => {
         <Route
           path='/ingredients/:id'
           element={
-            <Modal title='' onClose={handleClose}>
+            <Modal title='Детали ингредиента' onClose={handleClose} >
               <IngredientDetails />
             </Modal>
           }
